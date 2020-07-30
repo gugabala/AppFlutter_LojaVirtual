ed1f73a891218fb63f26d982a9ded6076a080c3c
gugabala <gugabala@gmail.com>
2020-07-30 14:55:21 -0300

    Grade de Imagens

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -45,12 +47,20 @@ class HomeTab extends StatelessWidget {
                   );
                 } else {
                   print(snapshort.data.documents.length);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200,
-                      alignment: Alignment.center,
-                      child: Container(),
-                    ),
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 1,
+                    mainAxisSpacing: 1,
+                    staggeredTiles: snapshort.data.documents.map((doc) {
+                      return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                    }).toList(),
+                    children: snapshort.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data['image'],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
